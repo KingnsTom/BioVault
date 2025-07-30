dca8692c55aac827872c6654e738461dca4aea24
KingnsTom <phonesbackupz@gmail.com>
2025-07-30 20:19:42 +0300

    My blog

--- Dev/my-blog/biovaoult-health-blog/biovault-health-blog-frontend/sanity/sanity.client.ts
+++
@@ -1,9 +0,0 @@
-// lib/sanity.client.ts
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-  projectId: "4yh8v1vf",
-  dataset: "production",
-  apiVersion: "2023-07-22",
-  useCdn: true,
-});
